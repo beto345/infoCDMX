3647a3b466649353ca924504c0f7e11bcaa01557
beto_ve_ <beto170820@gmail.com>
2026-04-06 14:13:51 -0600

    Pantalla de login

---
+++ app/src/main/java/com/example/infocdmx/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.example.infocdmx.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
